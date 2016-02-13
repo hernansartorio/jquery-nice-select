hernansartorio/jquery-nice-select

commit a353ca65baf663744a305eb9dd7d81e3b5dfc952

Fix disabled with class bug

js/jquery.nice-select.js
@@ -14,9 +14,9 @@
       var select = $(this);
       
       if (!select.next().hasClass('nice-select')) {
-        select.after('<div class="nice-select ' +
-          (select.attr('class') || '') +
-          (select.attr('disabled') ? 'disabled' : '" tabindex="0') +
+        select.after('<div class="nice-select' +
+          (' ' + select.attr('class') || '') +
+          (select.attr('disabled') ? ' disabled' : '" tabindex="0') +
           '"><span class="current"></span><ul class="list"></ul></div>');
         
         var dropdown = select.next();

commit e363a84f966f36d1a3320852d192bd17739475a4

bug fixed: select input with no 'class' attribute caused adding 'undefined' class to the dropdown container (caused by commit a353ca6) and added unnecessary space; bug fixed: use within a 'label' tag caused a double-click effect (dropdown not shown); updated: use of 'prop' function to check disabled state; added events: update (fixes #7), disable, enable (fixes #3); CSS: added vendor prefixes to arrow transform property (old IE, Safari) and dropdown opacity (IE<10) (fixes #1, fixes #10)

css/nice-select.css
@@ -42,9 +42,9 @@
     right: 12px;
     top: 50%;
     -webkit-transform-origin: 66% 66%;
-            transform-origin: 66% 66%;
+    transform-origin: 66% 66%;
     -webkit-transform: rotate(45deg);
-            transform: rotate(45deg);
+    transform: rotate(45deg);
     -webkit-transition: all 0.15s ease-in-out;
     transition: all 0.15s ease-in-out;
     width: 5px; }
@@ -52,6 +52,8 @@
     -webkit-transform: rotate(-135deg);
             transform: rotate(-135deg); }
   .nice-select.open .list {
+    filter: alpha(opacity=100);
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
     opacity: 1;
     pointer-events: auto;
     -webkit-transform: scale(1) translateY(0);
@@ -88,6 +90,8 @@
     box-shadow: 0 0 0 1px rgba(68, 68, 68, 0.11);
     box-sizing: border-box;
     margin-top: 4px;
+    filter: alpha(opacity=0);
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
     opacity: 0;
     overflow: hidden;
     padding: 0;

css/style.css
@@ -42,9 +42,9 @@
     right: 12px;
     top: 50%;
     -webkit-transform-origin: 66% 66%;
-            transform-origin: 66% 66%;
+    transform-origin: 66% 66%;
     -webkit-transform: rotate(45deg);
-            transform: rotate(45deg);
+    transform: rotate(45deg);
     -webkit-transition: all 0.15s ease-in-out;
     transition: all 0.15s ease-in-out;
     width: 5px; }
@@ -52,6 +52,8 @@
     -webkit-transform: rotate(-135deg);
             transform: rotate(-135deg); }
   .nice-select.open .list {
+    filter: alpha(opacity=100);
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
     opacity: 1;
     pointer-events: auto;
     -webkit-transform: scale(1) translateY(0);
@@ -88,6 +90,8 @@
     box-shadow: 0 0 0 1px rgba(68, 88, 112, 0.11);
     box-sizing: border-box;
     margin-top: 4px;
+    filter: alpha(opacity=0);
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
     opacity: 0;
     overflow: hidden;
     padding: 0;

js/jquery.nice-select.js
@@ -9,14 +9,15 @@
     // Hide native select
     this.hide();
 
-    // Create custom markup
+    // Create custom markup & select input events
     this.each(function() {
       var select = $(this);
       
       if (!select.next().hasClass('nice-select')) {
-        select.after('<div class="nice-select' +
-          (' ' + select.attr('class') || '') +
-          (select.attr('disabled') ? ' disabled' : '" tabindex="0') +
+        
+        var classAttr = select.attr('class');
+        select.after('<div class="nice-select' + (classAttr ? ' ' + classAttr : '') +
+          (select.prop('disabled') ? ' disabled' : '" tabindex="0') +
           '"><span class="current"></span><ul class="list"></ul></div>');
         
         var dropdown = select.next();
@@ -31,8 +32,28 @@
             ($(this).is(':selected') ? ' selected' : '') +
             ($(this).is(':disabled') ? ' disabled' : '') +
             '" data-value="' + $(this).val() + '"' +
-            (display ? ' data-display="' + display : '') + '"' +
+            (display ? ' data-display="' + display + '"' : '') +
             '>' + $(this).text() + '</li>');
+        });
+        
+        
+        /*
+           Events
+        */
+        
+        // Updates the dropdown after setting the value manually
+        select.on('update', function() {
+          dropdown.find('.list li[data-value="' + $(this).val() + '"]').trigger('click', [true]);
+        });
+        
+        // Sets disabled state
+        select.on('disable', function() {
+          dropdown.addClass('disabled');
+        });
+        
+        // Sets enabled state
+        select.on('enable', function() {
+          dropdown.removeClass('disabled');
         });
       }
     });
@@ -60,13 +81,14 @@
     
     // Close when clicking outside
     $(document).on('click.nice_select', function(event) {
-      if ($(event.target).closest('.nice-select').length === 0) {
+      var target = $(event.target);
+      if ((target.closest('.nice-select').length === 0) && target.is(':visible')) {
         $('.nice-select').removeClass('open').find('.option');  
       }
     });
     
     // Option click
-    $(document).on('click.nice_select', '.nice-select .option:not(.disabled)', function(event) {
+    $(document).on('click.nice_select', '.nice-select .option:not(.disabled)', function(event, stopPropagation) {
       var option = $(this);
       var dropdown = option.closest('.nice-select');
       
@@ -77,6 +99,9 @@
       dropdown.find('.current').text(text);
       
       dropdown.prev('select').val(option.data('value')).trigger('change');
+      
+      if (stopPropagation)
+        event.stopPropagation();
     });
 
     // Keyboard events
@@ -128,7 +153,7 @@
         }
       }
     });
-
+    
   };
 
 }(jQuery));

scss/nice-select.scss
@@ -57,7 +57,11 @@ $arrow_color: $gray !default;
     position: absolute; 
     right: 12px; 
     top: 50%; 
+    -ms-transform-origin: 66% 66%;
+    -webkit-transform-origin: 66% 66%;
     transform-origin: 66% 66%;
+    -ms-transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
     transform: rotate(45deg);
     transition: all 0.15s ease-in-out;
     width: 5px;
@@ -69,7 +73,9 @@ $arrow_color: $gray !default;
       transform: rotate(-135deg);
     }
     .list {
-      opacity: 1;
+      filter: alpha(opacity=100);
+       -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+       opacity: 1;
       pointer-events: auto;
       transform: scale(1) translateY(0);
     }
@@ -119,6 +125,8 @@ $arrow_color: $gray !default;
     box-shadow: 0 0 0 1px rgba($gray_dark, .11);
     box-sizing: border-box;
     margin-top: 4px;
+    filter: alpha(opacity=0);
+     -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
     opacity: 0;
     overflow: hidden;
     padding: 0;