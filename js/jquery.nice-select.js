bded7a8a155d73463622966133828a76c5e3c6c5
Hernán OSX <hernan124@gmail.com>
2016-05-09 01:17:24 -0300

    Add update and destroy methods

--- js/jquery.nice-select.js
+++ js/jquery.nice-select.js
@@ -4,47 +4,85 @@
  
 (function($) {
 
-  $.fn.niceSelect = function() {
+  $.fn.niceSelect = function(method) {
+    
+    // Methods
+    if (typeof method == 'string') {      
+      if (method == 'update') {
+        this.each(function() {
+          var $select = $(this);
+          var $dropdown = $(this).next('.nice-select');
+          var open = $dropdown.hasClass('open');
+          
+          if ($dropdown.length) {
+            $dropdown.remove();
+            create_nice_select($select);
+            
+            if (open) {
+              $select.next().trigger('click');
+            }
+          }
+        });
+      } else if (method == 'destroy') {
+        this.each(function() {
+          var $select = $(this);
+          var $dropdown = $(this).next('.nice-select');
+          
+          if ($dropdown.length) {
+            $dropdown.remove();
+            $select.css('display', '');
+          }
+        });
+        if ($('.nice-select').length == 0) {
+          $(document).off('.nice_select');
+        }
+      } else {
+        console.log('Method "' + method + '" does not exist.')
+      }
+      return this;
+    }
       
     // Hide native select
     this.hide();
-
+    
     // Create custom markup
     this.each(function() {
       var $select = $(this);
       
       if (!$select.next().hasClass('nice-select')) {
-        
-        $select.after($('<div></div>')
-          .addClass('nice-select')
-          .addClass($select.attr('class') || '')
-          .addClass($select.attr('disabled') ? 'disabled' : '')
-          .attr('tabindex', $select.attr('disabled') ? null : '0')
-          .html('<span class="current"></span><ul class="list"></ul>')
-        );
-        
-        var $dropdown = $select.next();
-        var $options = $select.find('option');
-        var $selected = $select.find('option:selected');
-        
-        $dropdown.find('.current').html($selected.data('display') || $selected.text());
-        
-        $options.each(function(i) {
-          var $option = $(this);
-          var display = $option.data('display');
-
-          $dropdown.find('ul').append($('<li></li>')
-            .attr('data-value', $option.val())
-            .attr('data-display', (display || null))
-            .addClass('option' +
-              ($option.is(':selected') ? ' selected' : '') +
-              ($option.is(':disabled') ? ' disabled' : ''))
-            .html($option.text())
-          );
-          
-        });
+        create_nice_select($select);
       }
     });
+    
+    function create_nice_select($select) {
+      $select.after($('<div></div>')
+        .addClass('nice-select')
+        .addClass($select.attr('class') || '')
+        .addClass($select.attr('disabled') ? 'disabled' : '')
+        .attr('tabindex', $select.attr('disabled') ? null : '0')
+        .html('<span class="current"></span><ul class="list"></ul>')
+      );
+        
+      var $dropdown = $select.next();
+      var $options = $select.find('option');
+      var $selected = $select.find('option:selected');
+      
+      $dropdown.find('.current').html($selected.data('display') || $selected.text());
+      
+      $options.each(function(i) {
+        var $option = $(this);
+        var display = $option.data('display');
+
+        $dropdown.find('ul').append($('<li></li>')
+          .attr('data-value', $option.val())
+          .attr('data-display', (display || null))
+          .addClass('option' +
+            ($option.is(':selected') ? ' selected' : '') +
+            ($option.is(':disabled') ? ' disabled' : ''))
+          .html($option.text())
+        );
+      });
+    }
     
     /* Event listeners */
     
@@ -144,6 +182,8 @@
     if (style.pointerEvents !== 'auto') {
       $('html').addClass('no-csspointerevents');
     }
+    
+    return this;
 
   };
 
