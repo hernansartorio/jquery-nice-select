58464019ac79ae5d483dbdd3e2ceaa060a9b71d9
Hampus <hampus@crealevant.se>
2018-03-28 13:38:31 +0200

    Possibility to add image inside select with attribute data-image for every option

--- js/jquery.nice-select.js
+++ js/jquery.nice-select.js
@@ -1,23 +1,23 @@
 /*  jQuery Nice Select - v1.1.0
     https://github.com/hernansartorio/jquery-nice-select
     Made by Hernán Sartorio  */
- 
+
 (function($) {
 
   $.fn.niceSelect = function(method) {
-    
+
     // Methods
-    if (typeof method == 'string') {      
+    if (typeof method == 'string') {
       if (method == 'update') {
         this.each(function() {
           var $select = $(this);
           var $dropdown = $(this).next('.nice-select');
           var open = $dropdown.hasClass('open');
-          
+
           if ($dropdown.length) {
             $dropdown.remove();
             create_nice_select($select);
-            
+
             if (open) {
               $select.next().trigger('click');
             }
@@ -27,7 +27,7 @@
         this.each(function() {
           var $select = $(this);
           var $dropdown = $(this).next('.nice-select');
-          
+
           if ($dropdown.length) {
             $dropdown.remove();
             $select.css('display', '');
@@ -41,19 +41,19 @@
       }
       return this;
     }
-      
+
     // Hide native select
     this.hide();
-    
+
     // Create custom markup
     this.each(function() {
       var $select = $(this);
-      
+
       if (!$select.next().hasClass('nice-select')) {
         create_nice_select($select);
       }
     });
-    
+
     function create_nice_select($select) {
       $select.after($('<div></div>')
         .addClass('nice-select')
@@ -62,75 +62,76 @@
         .attr('tabindex', $select.attr('disabled') ? null : '0')
         .html('<span class="current"></span><ul class="list"></ul>')
       );
-        
+
       var $dropdown = $select.next();
       var $options = $select.find('option');
       var $selected = $select.find('option:selected');
-      
-      $dropdown.find('.current').html($selected.data('display') || $selected.text());
-      
+
+        $dropdown.find('.current').html($selected.data('display') || "<img src='" + $selected.attr('data-image') + "' width='50'>" + $selected.text());
+
       $options.each(function(i) {
         var $option = $(this);
         var display = $option.data('display');
 
         $dropdown.find('ul').append($('<li></li>')
           .attr('data-value', $option.val())
+          .attr('data-image', $option.attr('data-image'))
           .attr('data-display', (display || null))
           .addClass('option' +
             ($option.is(':selected') ? ' selected' : '') +
             ($option.is(':disabled') ? ' disabled' : ''))
-          .html($option.text())
+            .html("<img src='" + $option.attr('data-image') + "' width='50'>" + $option.text())
         );
       });
     }
-    
+
     /* Event listeners */
-    
+
     // Unbind existing events in case that the plugin has been initialized before
     $(document).off('.nice_select');
-    
+
     // Open/close
     $(document).on('click.nice_select', '.nice-select', function(event) {
       var $dropdown = $(this);
-      
+
       $('.nice-select').not($dropdown).removeClass('open');
       $dropdown.toggleClass('open');
-      
+
       if ($dropdown.hasClass('open')) {
-        $dropdown.find('.option');  
+        $dropdown.find('.option');
         $dropdown.find('.focus').removeClass('focus');
         $dropdown.find('.selected').addClass('focus');
       } else {
         $dropdown.focus();
       }
     });
-    
+
     // Close when clicking outside
     $(document).on('click.nice_select', function(event) {
       if ($(event.target).closest('.nice-select').length === 0) {
-        $('.nice-select').removeClass('open').find('.option');  
+        $('.nice-select').removeClass('open').find('.option');
       }
     });
-    
+
     // Option click
     $(document).on('click.nice_select', '.nice-select .option:not(.disabled)', function(event) {
       var $option = $(this);
       var $dropdown = $option.closest('.nice-select');
-      
+
       $dropdown.find('.selected').removeClass('selected');
       $option.addClass('selected');
-      
-      var text = $option.data('display') || $option.text();
-      $dropdown.find('.current').text(text);
-      
+
+      var text = $option.data('display') || "<img src='" + $option.attr('data-image') + "' width='50'>" + $option.text();
+      $dropdown.find('.current').html(text);
+
       $dropdown.prev('select').val($option.data('value')).trigger('change');
     });
 
     // Keyboard events
-    $(document).on('keydown.nice_select', '.nice-select', function(event) {    
+    $(document).on('keydown.nice_select', '.nice-select', function(event) {
       var $dropdown = $(this);
       var $focused_option = $($dropdown.find('.focus') || $dropdown.find('.list .option.selected'));
-      
+
       // Space or Enter
       if (event.keyCode == 32 || event.keyCode == 13) {
         if ($dropdown.hasClass('open')) {
@@ -182,7 +183,7 @@
     if (style.pointerEvents !== 'auto') {
       $('html').addClass('no-csspointerevents');
     }
-    
+
     return this;
 
   };
